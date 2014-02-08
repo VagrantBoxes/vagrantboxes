cc1fbbcda1ddbc42865af91a7fff6450b7801672
slopjong <git@slopjong.de>
2014-02-08 18:42:46 +0100

    Implemented a menu controller and some more adaptions done

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,59 @@
 'use strict';
 
-angular.module('vagrantlistApp', [
-  'ngResource',
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
+// create the app module
+var App = angular.module('vagrantlistApp', ['ngResource', 'ngRoute']);
+
+// configure the routes for the newly created module
+App.config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'ListController'
+        })
+        .when('/suggest', {
+            templateUrl: 'views/suggest.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+// initialize the menu controller to change the 'active' state
+App.controller(
+    'MenuController',
+    function ($scope) {
+
+        /**
+         * Index of the current active menu item
+         * @type {number}
+         */
+        $scope.current_active = 0;
+
+        /**
+         * Menu items being populated at runtime
+         * @type {Array}
+         */
+        $scope.items = [
+            {
+                text: "Boxes",
+                route: "/",
+                active: true
+            },
+            {
+                text: "Suggest a box",
+                route: "/suggest",
+                active: false
+            }
+        ];
+
+        /**
+         * Change the active state when a menu item is clicked
+         * @param index
+         */
+        $scope.click = function(index) {
+            $scope.items[$scope.current_active].active = false;
+            $scope.items[index].active = true;
+            $scope.current_active = index;
+        }
+    }
+);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,7 @@
 'use strict';
 
-angular.module('vagrantlistApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('vagrantlistApp').controller(
+    'ListController',
+    function ($scope) {
+    }
+);
