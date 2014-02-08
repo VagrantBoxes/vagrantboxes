02551067f6f54842aa0a4448ce46cd6e83e609ab
slopjong <git@slopjong.de>
2014-02-08 23:17:19 +0100

    Docblocks added and evaluating a ng-show expression

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,8 +4,32 @@ angular.module('vagrantlistApp').controller(
     'ListController',
     function ($scope, $http, DebianBoxes) {
 
+        /**
+         * An array of objects of the form { "name": "Debian", "slug": "debian" }
+         * @type {Array}
+         */
         $scope.distributions = [];
+
+        /**
+         * An array with all the boxes fetched from the server
+         * @type {Array}
+         */
         $scope.boxes = [];
+
+        /**
+         * Distributions that should be displayed
+         * @type {Array}
+         */
+        var distributions_slug_show = [];
+
+        /**
+         * Handler for the ng-show attribute on a box row
+         * @param slug
+         * @returns {boolean}
+         */
+        $scope.show = function(slug) {
+            return -1 !== distributions_slug_show.indexOf(slug);
+        };
 
         $http
             .get('boxes/_distributions.json')
@@ -14,7 +38,15 @@ angular.module('vagrantlistApp').controller(
                 $scope.distributions = distributions;
 
                 for(var i = 0; i < distributions.length; i++) {
+
                     var slug = distributions[i].slug;
+
+                    // initially every distro will be shown
+                    distributions_slug_show.push(slug);
+
+                    // now populate the boxes array while the distribtion
+                    // information will be merged into the jsons returned
+                    // by the server
                     $http
                         .get('boxes/' + slug + '.json')
                         .success(
