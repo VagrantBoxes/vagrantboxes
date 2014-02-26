fd63be85aba4fc5cc1e5b4bea408a784b204592d
slopjong <git@slopjong.de>
2014-02-26 22:00:09 +0100

    Directives implemented, code = mess

---
+++ app/scripts/directives/directives.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var directives = angular.module('vagrantlistApp.directives', []);
+
+// the following directives are there for experiment purposes only
+// in order to understand when a jquery plugin should be initialized
+// which is not so obvious with
+directives.directive('picker', function($compile, $timeout) {
+    return function postLink(scope, iElement, iAttrs) {
+//        jQuery(iElement).closest('select').selectpicker('refresh');
+//        console.log(jQuery(iElement).closest('select'));
+//        console.log('postlink');
+        $timeout(function () {
+            jQuery(iElement).closest('select').selectpicker('refresh');
+        }, 200);
+    }
+    return {
+        restrict : 'A',
+        link: function(scope, element, attrs) {
+            console.log('sp1 link');
+        },
+        compile : function(tele, tattr) {
+            console.log("sp1 compile");
+//            tele.selectpicker();
+            return {
+                pre : function(scope, iele, iattr) {
+//                    console.log("sp1 compile pre");
+                },
+                post : function(scope, iele, iattr) {
+//                    console.log("sp1 compile post");
+//                    jQuery(iele).closest('select').selectpicker('refresh');
+                    $timeout(function () {
+                        jQuery(iele).closest('select').selectpicker('refresh');
+                    }, 100);
+                }
+            };
+        }
+    };
+});
+
+directives.directive('directive2', function($compile) {
+    return function preLink(scope, iElement, iAttrs) {
+        console.log('postlink2');
+    }
+    return {
+        restrict : 'A',
+        link: function(scope, element, attrs) {
+            console.log('sp2 link');
+        },
+        compile : function(tele, tattr) {
+            console.log("sp2 compile");
+            return {
+                pre : function(scope, iele, iattr) {
+                    console.log("sp2 compile pre");
+                },
+                post : function(scope, iele, iattr) {
+                    console.log("sp2 compile post");
+                }
+            };
+        }
+    };
+});
