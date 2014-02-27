65ff85911826cfecb95ee09c22d2f05e1df00654
slopjong <git@slopjong.de>
2014-02-27 15:35:20 +0100

    Dist sources added. Boxes were missing after building.

--- Gruntfile.js
+++ Gruntfile.js
@@ -255,6 +255,7 @@ module.exports = function (grunt) {
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
+            'boxes/*.json',
             'bower_components/**/*',
             'images/{,*/}*.{webp}',
             'fonts/*'
